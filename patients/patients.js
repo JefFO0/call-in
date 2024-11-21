5be1024fc66f3d4d0d88e4062c2c07587d4125b7
Jeffrey Oppong <jeffreyoppong42@gmail.com>
2024-11-21 15:12:27 +0000

    make correct changes

--- patients/patients.js
+++ patients/patients.js
@@ -1,2 +1,58 @@
 const express = require("express");
 const app = express();
+const patientsRouter = express.Router();
+const bodyParser = require("body-parser");
+
+
+let listOfPatients = ["Kwame", "Bougie"]; // CALL FROM DB
+
+app.use("/patients", patientsRouter);
+app.use(bodyParser.json());
+patientsRouter.param("id", (req, res, next, id) => {
+  try {
+    if (listOfPatients[id]) {
+      req.patient = listOfPatients[id];
+      res.status(200).send(req.patient);
+      next();
+    } else {
+      throw Error("This patient is not in the records");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET ALL PATIENTS
+patientsRouter.get("/", (req, res, next) => {
+  res.send(listOfPatients);
+});
+
+// GET UNIQUE PATIENT
+patientsRouter.get("/:id", (req, res, next) => {
+  res.send(listOfPatients[req.patient]);
+});
+
+// CREATE NEW PATIENT
+// patientsRouter.post("/createPatient", (req, res, next) => {
+//   const update = req.query;
+//   const correct = createElement(animals, update);
+//   if (correct) {
+//     animals.push(correct);
+//     res.status(201).send(correct);
+//   } else {
+//     res.status(400).send("Error creating new post");
+//   }
+// });
+// UPDATE PATIENT PROFILE
+
+// DELETE PATIENT PROFILE
+
+// ERROR HANDLER
+const errorHandler = (err, req, res, next) => {
+  let errorStatus = err.status;
+  if (!errorStatus) {
+    errorStatus = 500;
+  }
+  res.status(errorStatus).send(err.message);
+};
+app.use(errorHandler);
